0bc53b4c610a7fe89fa0e634a3710c7d6124a6f9
BelleChiu <bellechiu@gmail.com>
2024-09-19 04:00:46 +0000

    postgre db creation

---
+++ backend-flask/db/schema.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE public.users (
+  uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  display_name text,
+  handle text
+  cognito_user_id text,
+  created_at TIMESTAMP default current_timestamp NOT NULL
+);
+
+
